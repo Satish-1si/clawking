c950f85001eb88a705b22938e4f153ba2d7fe590
satish manepalli <manepallisatish11@gmail.com>
2025-08-31 00:46:38 +0530

    chages in src

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -3,9 +3,19 @@
 import { useNavigate } from "react-router-dom"; // React Router
 // import { PlayFill } from "react-bootstrap-icons";
 // import "../css/Game.css";
-import ClawKing  from "./View/claw.jsx"
+import ClawKing  from "./View/wensocket.jsx"
 const Game = () => {
   const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   // Set timer to navigate back after 10 minutes (600000 ms)
+  //   const timer = setTimeout(() => {
+  //     navigate(-1); // Go back to previous page
+  //   }, 3000);
+
+  //   // Cleanup timer if component unmounts
+  //   return () => clearTimeout(timer);
+  // }, [navigate]);
 
   return (
 
